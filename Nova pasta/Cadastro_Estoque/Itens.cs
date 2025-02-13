d9a9a1479b20d727a5562c801d8e541447ebc2a5
Rafaela Pereira da Silva <pereira.r@estudante.ifro.edu.br>
2025-02-13 12:30:19 -0400

    Cadastro de do metodo Insert

---
+++ Nova pasta/Cadastro_Estoque/Itens.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro_Estoque
+{
+    internal class Itens
+    {
+        public string nome;
+        public string modelo;
+        public string tipo;
+        public DateTime dataEntrada;
+        public DateTime dataSaida;
+        public double valor;
+
+    }
+}
